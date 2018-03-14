333075332a3fed2f0ab5f94c699ce956de796b3c
Marcos Moritz <marcosmoritz@gmail.com>
2018-03-14 00:23:19 -0300

    Add a more modular method to get a repo and run a callback

--- main.el
+++ main.el
@@ -1,23 +1,14 @@
 (require 'oauth2)
 
-(defun moritz/get-repository (user callback)
+(defun moritz/get-repository (user callback &optional cbargs)
   "List bitbucket user's repositories"
   (let ((url-request-method "GET")
         (endpoint "repositories/%s"))
     (oauth2-url-retrieve
      moritz/bitbucket--token
      (concat moritz/bitbucket--v2 (format endpoint user))
-     callback)))
-
-(defun moritz/get-repository (user callback)
-  "List bitbucket user's repositories"
-  (let ((url-request-method "GET")
-        (endpoint "repositories/%s"))
-    (oauth2-url-retrieve
-     moritz/bitbucket--token
-     (concat moritz/bitbucket--v2 (format endpoint user))
-     callback)))
-
+     callback
+     cbargs)))
 
 (defun moritz/parse-repositories (result)
   (let ((request-data (moritz/parse-json))
@@ -27,8 +18,8 @@
             (cdr (assoc 'values request-data)))
     repo-names))
 
-
-(defun moritz/select-repository (result)
+(defun moritz/select-repository (result &optional cbargs)
+  (message cbargs)
   (let ((data (moritz/parse-json)))
     (let ((repositories-helm-source
            `((name . "Select the repository: ")
@@ -39,6 +30,27 @@
                          (moritz/do-something (moritz/get-repo-uuid (moritz/get-repo-by-name data candidate))))))))
       (helm :sources '(repositories-helm-source)))))
 
+(defun moritz/select-repo-and-run-action (result &optional callback)
+  (let ((data (moritz/parse-json)))
+    (let ((repositories-helm-source
+           `((name . "Select the repository: ")
+             (candidates . ,(mapcar '(lambda (element)
+                                       (cdr (assoc 'name element)))
+                                    (cdr (assoc 'values data))))
+             (action . (lambda (candidate)
+                         (funcall callback (moritz/get-repo-by-name data candidate)))))))
+      (helm :sources '(repositories-helm-source)))))
+
+(defun moritz/select-repository-and-get-url (result)
+  (let ((data (moritz/parse-json)))
+    (let ((repositories-helm-source
+           `((name . "Select the repository: ")
+             (candidates . ,(mapcar '(lambda (element)
+                                       (cdr (assoc 'name element)))
+                                    (cdr (assoc 'values data))))
+             (action . (lambda (candidate)
+                         (moritz/do-something (moritz/get-repo-ssh-url (moritz/get-repo-by-name data candidate))))))))
+      (helm :sources '(repositories-helm-source)))))
 
 ;; parsing utils
 (defun moritz/get-repo-uuid (repo)
@@ -48,7 +60,8 @@
   (message uuid))
 
 (defun moritz/get-repo-by-name (repos-vector name)
-  (let (;; convert vector to list
+
+  (let ( ;; convert vector to list
         (repo-list (append (cdr (assoc 'values repos-vector)) nil))
         (value))
     (while (and repo-list (not value))
@@ -64,23 +77,50 @@
   (append '() (json-read)))
 
 ;; tests and examples
-(moritz/list-pull-requests "ptmtech" "portaltm.server")
+;; (moritz/list-pull-requests "ptmtech" "portaltm.server")
+
+(moritz/get-repository "mmoritz" 'moritz/select-repository '("test1"))
+
+;; get repo and call a method on it
+(moritz/get-repository "mmoritz"
+                       'moritz/select-repo-and-run-action
+                       '((lambda (repo) (message (moritz/get-repo-uuid repo)))))
+
 
 (moritz/get-repository "ptmtech" 'moritz/select-repository)
 
-(moritz/get-repository "ptmtech" 'moritz/select-repository)
+(moritz/get-remote "mmoritz" 'moritz/select-repository)
+(moritz/get-repository "mmoritz" 'moritz/select-repository-and-get-url)
 
-my-data-after-parsing-json
-(insert (format "%s"  (assoc 'values my-data-after-parsing-json)))
-(cdr (assoc 'name (elt (cdr (assoc 'values my-data-after-parsing-json)) 0)))
-
-(cdr (assoc 'name (elt (cdr (assoc 'values my-data-after-parsing-json)) 0)))
-
-(cdr (assoc 'name (elt (cdr (assoc 'values my-data-after-parsing-json)) 0)))
-
-(assoc 'values my-data-after-parsing-json)
 
 ;; ideas
 ;; magit - add a button to bitbucket
 ;; pr - list pr's for this project
 ;; diff-pr - show the diff between merging branches
+
+
+(insert (format "%s" teste-repo))
+
+(defun moritz/get-repo-https-url (repo)
+  (cdr (car (car (append (cdr (assoc 'clone
+                                     (assoc 'links repo)))
+                         nil)))))
+
+(defun moritz/get-repo-ssh-url (repo)
+  (cdr (car (car (cdr (append (cdr (assoc 'clone
+                                          (assoc 'links teste-repo)))
+                              nil))))))
+
+(get-repo-https-url teste-repo)
+(get-repo-ssh-url teste-repo)
+
+(insert (format "%s" (cdr (car (car (cdr (append (cdr (assoc 'clone
+                                                             (assoc 'links teste-repo)))
+                                                 nil)))))))
+
+(href . https://mmoritz@bitbucket.org/mmoritz/flute-friend.git)
+
+[((href . https://mmoritz@bitbucket.org/mmoritz/flute-friend.git) (name . https))
+ ((href . git@bitbucket.org:mmoritz/flute-friend.git) (name . ssh))]
+
+(clone . [((href . https://mmoritz@bitbucket.org/mmoritz/flute-friend.git) (name . https)) ((href . git@bitbucket.org:mmoritz/flute-friend.git) (name . ssh))])
