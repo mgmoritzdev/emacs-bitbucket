25c80ead3ff62030767fdcc6acf46f3ac8655ac8
Marcos Moritz <moritz@portaltelemedicina.com.br>
2018-03-31 06:05:02 -0300

    Add a call to commits

---
+++ commits.el
@@ -0,0 +1,16 @@
+(defun moritz/select-commits-and-run-action (result &optional callback)
+  (let ((data (moritz/parse-json)))
+    (let ((commits-helm-source
+           `((name . "Select a commit: ")
+             (candidates . ,(mapcar '(lambda (element)
+                                       `(,(car (split-string
+                                                (cdr (assoc 'raw (assoc 'summary element)))
+                                                "\\\n")) . ,element))
+                                    (cdr (assoc 'values data))))
+             (action . (lambda (candidate)
+                         (funcall callback candidate))))))
+      (helm :sources '(commits-helm-source)))))
+
+
+(defun moritz/run-commits-action (commit)
+  (message (cdr (assoc 'hash commit))))
